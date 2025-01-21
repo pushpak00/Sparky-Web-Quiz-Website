5dd195d2c67bf6ebf0f7dc980daf0db7b8c61cf3
Pushpak Paunikar <54863992+pushpak00@users.noreply.github.com>
2025-01-21 18:53:35 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+// define the quiz data
+
+const quizData = [
+    {
+        question: "What does HTML stands for?",
+        options: [
+            "Hypertext Markup Language",
+            "Hyper Transfer Markup Language",
+            "Hypertext Machine Language",
+            "Hyperlink and Text Markup Language"
+        ],
+        correct: 0
+    },
+    {
+        question: "Which CSS property is used to control the spacing between elements?",
+        options: ["margin", "padding", "spacing", "border-spacing"],
+        correct: 1,
+    },
+    {
+        question: "What is the JavaScript function used to select an HTML element by it's ID?",
+        options: [
+            "document.query",
+            "getElementById",
+            "selectElement",
+            "findElementById"
+        ], 
+        correct: 1,
+    },
+    {
+        question: "In React.js, which hook is used to perform side effects in a function components?",
+        options: ["useEffect", "useState", "useContext", "useReducer"],
+        correct: 0,
+    },
+    {
+        question: "Which HTML tag is used to create an ordered list?",
+        options: ["<ul>", "<li>", "<ol>", "<dl>"],
+        correct: 2,
+    }
+
+]
